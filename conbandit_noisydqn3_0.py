Changed pace to 1, set state sampling function to uniform, added proper regret calculation. Accidentally calcuted it after redrawing state and arms; fixed now.

conbandit_noisydqn3_0.py
@@ -62,7 +62,7 @@ class Args:
     states: int = 2
     optimal_arms: int | list[int] = 1
     dynamic_rate: int | None = None
-    pace: int = 5
+    pace: int = 1
     optimal_mean: float = 10
     optimal_std: float = 1
     min_suboptimal_mean: float = 0
@@ -181,20 +181,21 @@ class DQNAgent:
 
     def step(self, state: np.ndarray, action: np.ndarray) -> float:
         """Take an action and return the response of the env."""
-        next_state, reward, _, _, _ = self.env.step(action)
+        next_state, reward, _, _, info = self.env.step(action)
         
         if not self.is_test:
             self.memory.store(state, action, reward)
     
-        return next_state, reward
+        return next_state, reward, info
         
     def train(self, num_episodes: int):
         """Train the agent."""
         self.is_test = False
         
-        losses = []
         rewards = []
         scores = []
+        losses = []
+        regrets = []
         arm_weights = []
         data = []
         state, _ = self.env.reset(seed=self.args.seed)
@@ -202,15 +203,18 @@ class DQNAgent:
         # Double loop isn't necessary
         for step_id in tqdm(range(1, num_episodes + 1)):
             score = 0
-            
+
             self.dqn.resample_noise() # line 5
 
             action = self.select_action(state) # line 6
-            next_state, reward = self.step(state, action) # line 7
+            next_state, reward, info = self.step(state, action) # line 7
 
             data.append([step_id, float(state[0]), action, float(reward)]) # line 8
 
+            # regrets.append(self.env.unwrapped.reward(state, action) - reward) # !!! THIS DOESN'T WORK
+            regrets.append(info['regret'])
             rewards.append(reward)
+
             state = next_state
 
 
@@ -252,7 +256,7 @@ class DQNAgent:
         print(f"Mean rewards: {np.mean(rewards)}")
         if self.args.logging: wandb.run.summary["mean_rewards"] = np.mean(rewards)
         self.env.close()
-        if self.args.plotting: self._plot(scores, losses, arm_weights, np.array(data))
+        if self.args.plotting: self._plot(scores, losses, regrets, arm_weights, np.array(data))
         
     def test(self, episode_length) -> None:
         """Test the agent."""
@@ -266,7 +270,7 @@ class DQNAgent:
         
         for _ in range(episode_length):
             action = self.select_action(state)
-            next_state, reward = self.step(state, action)
+            next_state, reward, _ = self.step(state, action)
 
             state = next_state
             score += reward
@@ -298,22 +302,29 @@ class DQNAgent:
         self,
         scores: List[float], 
         losses: List[float],
+        regrets: List[float],
         arm_weights: List[np.ndarray],
         data: np.ndarray
     ):
         """Plot the training progresses."""
-        plt.figure(figsize=(15, 5))
-        plt.subplot(121)
+        plt.figure(figsize=(17, 5))
+        plt.subplot(131)
         plt.title('score: %s' % (np.mean(scores[-10:])))
         plt.plot(scores)
         plt.xlabel('Episode')
         plt.ylabel('Score')
 
-        plt.subplot(122)
+        plt.subplot(132)
         plt.title('loss')
         plt.plot(losses)
         plt.xlabel('Training Steps')
         plt.ylabel('Loss')
+
+        plt.subplot(133)
+        plt.title('regret')
+        plt.plot(regrets)
+        plt.xlabel('Training Steps')
+        plt.ylabel('Regret')
 
         # ------------------------------------------------------
 
@@ -439,7 +450,7 @@ if __name__ == "__main__":
         min_suboptimal_mean=args.min_suboptimal_mean,
         max_suboptimal_mean=args.max_suboptimal_mean,
         suboptimal_std=args.suboptimal_std,
-        noisy = True)
+        noisy = False)
 
     agent = DQNAgent(env, args)
 

custom_envs/conbandit_v2.py
@@ -30,6 +30,10 @@ class ConbanditEnv2(gym.Env):
         # std_dev = self.slopes[action]
         # return np.sum((1 / (std_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((state - mean) / std_dev) ** 2))
 
+    def optimal_reward(self, state: np.ndarray) -> int:
+        arr = [self.reward(state, action) for action in range(self.arms)]
+        return np.argmax(arr), np.max(arr)
+
     def __draw_arms(self):
         """
         Draw new arms
@@ -48,11 +52,11 @@ class ConbanditEnv2(gym.Env):
         self.slopes = self.rng.uniform(-1, 1, (self.arms, self.dims))
 
     def __draw_state(self): 
-        self.state = self.rng.normal(0, 1, self.dims)
-        # self.state = self.rng.uniform(-3, 3, 1)
+        # self.state = self.rng.normal(0, 1, self.dims)
+        self.state = self.rng.uniform(-3.0, 3.0, 1)
 
     def __init__(self, dims: int = 1, arms: int = 10, states: int = 2, optimal_arms: int | list[int] = 1,
-                 dynamic_rate: int | None = None, pace: int = 5, seed: int | None = None, optimal_mean: float = 10,
+                 dynamic_rate: int | None = None, pace: int = 1, seed: int | None = None, optimal_mean: float = 10,
                  optimal_std: float = 1, min_suboptimal_mean: float = 0, max_suboptimal_mean: float = 5,
                  suboptimal_std: float = 1, noisy: bool = False):
         """
@@ -129,6 +133,10 @@ class ConbanditEnv2(gym.Env):
         if self.noisy:
             reward = self.rng.normal(reward, 0.1, 1)[0]
 
+        # Calculate regret before redrawing state and arms!!
+        optimal_reward_action, optimal_reward = self.optimal_reward(self.state)
+        regret = optimal_reward - reward
+
         self.ssr += 1
         if self.pace is None or self.ssr % self.pace == 0:
             self.__draw_state()
@@ -140,4 +148,4 @@ class ConbanditEnv2(gym.Env):
                 self.seed += 1
             self.__draw_arms()
 
-        return np.array(self.state, dtype=np.float32), reward, False, False, {}
+        return np.array(self.state, dtype=np.float32), reward, False, False, {'regret': regret}