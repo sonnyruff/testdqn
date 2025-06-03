da69dd4a2c877c2e7bb22dda74ec90025ffb516f
Sonny <s.ruff@student.tudelft.nl>
2025-06-03 22:50:08 +0200

    Clean up

--- custom_envs/__init__.py
+++ custom_envs/__init__.py
@@ -1,7 +1,5 @@
 import gymnasium as gym
 from custom_envs.bandit_v0 import BanditEnv0
-from custom_envs.conbandit_v0 import ConbanditEnv0
-from custom_envs.conbandit_v1 import ConbanditEnv1
 from custom_envs.conbandit_v2 import ConbanditEnv2
 from custom_envs.mnistbandit_v0 import MNISTBanditEnv
 from custom_envs.nnbandit_v0 import NNBanditEnv
@@ -9,18 +7,6 @@ from custom_envs.nnbandit_v0 import NNBanditEnv
 gym.register(
     id='Bandit-v0',
     entry_point=__name__ + ":BanditEnv0",
-    max_episode_steps=1000
-)
-
-gym.register(
-    id='ContextualBandit-v0',
-    entry_point=__name__ + ":ConbanditEnv0",
-    max_episode_steps=1000
-)
-
-gym.register(
-    id='ContextualBandit-v1',
-    entry_point=__name__ + ":ConbanditEnv1",
     max_episode_steps=1000
 )
 
